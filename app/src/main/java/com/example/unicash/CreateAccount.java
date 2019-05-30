baaf874bec5ea4d3a3721c3c5b1442b1cb040125
Sinwa <sianwamak@gmail.com>
2019-05-30 16:59:51 +0300

    TESTING 123

--- app/src/main/java/com/example/unicash/CreateAccount.java
+++ app/src/main/java/com/example/unicash/CreateAccount.java
@@ -1,13 +1,43 @@
 package com.example.unicash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CreateAccount extends AppCompatActivity {
-
+    EditText _txtusername,_txtemail,_txtpass;
+    Button _btnreg;
+    TextView _btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        _txtusername= findViewById(R.id.username);
+        _txtemail= findViewById(R.id.email);
+        _txtpass= findViewById(R.id.password);
+        _btnlogin =findViewById(R.id.btnlog);
+        _btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CreateAccount.this,Login.class);
+                startActivity(intent);
+            }
+        });
+
+        _btnreg= findViewById(R.id.reg);
+        _btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createAccount();
+            }
+        });
+    }
+
+    private void createAccount() {
     }
 }
--- app/src/main/java/com/example/unicash/Login.java
+++ app/src/main/java/com/example/unicash/Login.java
@@ -1,13 +1,47 @@
 package com.example.unicash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    EditText _txtemail,_txtpass;
+    Button _btnlogin;
+    TextView _btnreg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        _btnlogin = findViewById(R.id.login);
+        _btnreg = findViewById(R.id.btnreg);
+
+        _txtemail = findViewById(R.id.email);
+        _txtpass = findViewById(R.id.password);
+
+        _btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        _btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, CreateAccount.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void login() {
+        //TODO ADD AUTHENTICATION CODE
     }
 }

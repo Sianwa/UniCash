5401ae83915b1b1643c2d7efa8d1b011308c39a2
Jesse <jesse.kiriungi@strathmore.edu>
2019-06-19 10:15:41 +0300

    reminders

--- app/src/main/java/com/example/unicash/MainActivity.java
+++ app/src/main/java/com/example/unicash/MainActivity.java
@@ -1,13 +1,58 @@
 package com.example.unicash;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class ReminderActivity extends ListActivity {
+    private ReminderListAdapter mAdapter;
+    private ReminderDBSupporter dbHelper = new ReminderDBSupporter(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); mContext = this;
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
+        setContentView(R.layout.reminder_list);
+        mAdapter = new ReminderListAdapter(this, dbHelper.getReminders());
+        setListAdapter(mAdapter);
+    }
+    public void setReminderEnabled(long id, boolean isEnabled) {
+        ReminderHelper.cancelReminders(this);
+        ReminderModel model = dbHelper.getReminder(id);
+        model.isEnabled = isEnabled;
+        dbHelper.updateReminder(model);
+        ReminderHelper.setReminders(this);
+    }
+    public void startReminderDetailsActivity(long id) {
+        Intent intent = new Intent(this, ReminderDetails.class);
+        intent.putExtra("id", id); startActivityForResult(intent, 0);
+    }
+    public void deleteReminder(long id) {
+        final long reminderId = id;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Please confirm")
+                .setTitle("Delete set?")
+                .setCancelable(true)
+                .setNegativeButton("Cancel", null)
+                .setPositiveButton("Ok", new OnClickListener() {
+
+                    @Override
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        ReminderHelper.cancelReminders(mContext);
+                        dbHelper.deleteReminder(reminderId);
+                        mAdapter.setReminders(dbHelper.getReminders());
+                        mAdapter.notifyDataSetChanged();
+                        ReminderHelper.setReminders(mContext);
+                    }
+                }).show();
     }
 }
+
---
+++ app/src/main/java/com/example/unicash/ReminderDetails.java
@@ -0,0 +1,58 @@
+package com.example.unicash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class ReminderDetails extends Activity {
+    private ReminderModel reminderDetails;
+    private TimePicker timePicker;
+    private EditText edtName;
+    private ReminderCustomSwitch chkWeekly;
+    private ReminderCustomSwitch chkSunday;
+    private TextView txtToneSelection;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reminder_desc);
+        timePicker = (TimePicker) findViewById(R.id.reminder_time_picker);
+        edtName = (EditText) findViewById(R.id.reminder_name);
+        chkWeekly = (ReminderCustomSwitch) findViewById(R.id.reminder_repeat_weekly);
+        chkSunday = (ReminderCustomSwitch) findViewById(R.id.reminder_repeat_sunday);
+        txtToneSelection = (TextView) findViewById(R.id.reminder_label_tone_selection);
+        chkWeekly.setChecked(reminderDetails.repeatWeekly);
+        chkSunday.setChecked(reminderDetails.getRepeatingDay(ReminderModel.SUNDAY));
+        txtToneSelection.setText(RingtoneManager.getRingtone(this, reminderDetails.reminderTone).getTitle(this));
+    }
+
+    final LinearLayout ringToneContainer = (LinearLayout) findViewById(R.id.reminder_ringtone_container);
+
+    ringToneContainer.setOnClickListener(new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+            startActivityForResult(intent , 1);
+        }
+    }
+
+
+private void updateModelFromLayout() {
+    reminderDetails.timeMinute = timePicker.getCurrentMinute().intValue();
+    reminderDetails.timeHour = timePicker.getCurrentHour().intValue();
+    reminderDetails.name = edtName.getText().toString();
+    reminderDetails.repeatWeekly = chkWeekly.isChecked();
+    reminderDetails.setRepeatingDay(ReminderModel.SUNDAY, chkSunday.isChecked());
+    reminderDetails.isEnabled = true;
+    }
+}
+

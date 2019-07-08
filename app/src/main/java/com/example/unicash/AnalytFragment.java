ec37945968a7a7a7ad858c3d6236ea5b9e66421b
Sinwa <sianwamak@gmail.com>
2019-07-08 14:06:10 +0300

    graphs need data

--- app/src/main/java/com/example/unicash/AnalytFragment.java
+++ app/src/main/java/com/example/unicash/AnalytFragment.java
@@ -4,11 +4,22 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class AnalytFragment extends Fragment {
+    private CardView mcardView;
+    private DatabaseReference reference;
+    private FirebaseDatabase database;
+
     View v;
     public AnalytFragment() {
     }
@@ -17,6 +28,27 @@ public class AnalytFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v=inflater.inflate(R.layout.analytics_frag,container,false);
+
+
+
+
+        //send query to firebasedb
+        database = FirebaseDatabase.getInstance();
+        reference= database.getReference().child("Expenses");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
         return v;
     }
 }
